7d2a30f3ca98cf3fc13e1c683b53a5cc9516f49c
LifeIsOne <didtmdgh258@gmail.com>
2025-05-22 21:23:16 +0900

    [CHORE] Bootstrap 설치 및 디렉토리 루트 설정

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+// Bootstrap += icons
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons'
+import 'bootstrap/dist/js/bootstrap.js'
+
 import './assets/main.css'
 
 import { createApp } from 'vue'
