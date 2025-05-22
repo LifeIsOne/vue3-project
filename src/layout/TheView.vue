c2b5a76f2e39f7120910aed39d4ae20ecba457a7
LifeIsOne <didtmdgh258@gmail.com>
2025-05-22 21:45:31 +0900

    [FEAT] 기본 레이아웃 생성

---
+++ src/layout/TheView.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <div class="container py-4">
+      <h1>Hello World 🙋‍♂️🌎</h1>
+    </div>
+  </main>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
