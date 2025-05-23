7b5082e7c25b6bf5551b1986343fc64ec0c0d085
LifeIsOne <didtmdgh258@gmail.com>
2025-05-23 23:43:35 +0900

    [FEAT] 라우터 매핑, 등록

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons'
 import 'bootstrap/dist/js/bootstrap.js'
+import router from './router'
 
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
