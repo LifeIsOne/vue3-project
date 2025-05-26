90c09814127606c769474ba4410918baab61979e
LifeIsOne <didtmdgh258@gmail.com>
2025-05-26 19:41:52 +0900

    [FEAT] `router`객체와 `useRouter`함수로 페이지 이동

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
     <h1>About View</h1>
+    <p>{{ $route.path }}</p>
+    <button @click="$router.push('/')" class="btn btn-primary">Home Button</button>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+console.log('route.path: ', route.path)
+</script>
 
 <style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <template>
   <div>
     <h1>Home View</h1>
+    <p>{{ $route.path }}</p>
+    <p>{{ $route.name }}</p>
+    <button @click="goAboutView" class="btn btn-primary">About Button</button>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const goAboutView = () => {
+  router.push('/about')
+}
+</script>
 
 <style lang="scss" scoped></style>
