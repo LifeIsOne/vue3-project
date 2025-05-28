6974243ecf3dadd1abc90936029a65874c45893e
LifeIsOne <didtmdgh258@gmail.com>
2025-05-28 21:19:56 +0900

    [FEAT] Nested Routes( 중첩 라우트 ) 적용

---
+++ src/views/nested/NestedView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ul class="nav nav-pills">
+      <li class="nav-item">
+        <RouterLink class="nav-link" active-class="active" to="/nested/one">Nested One</RouterLink>
+      </li>
+      <li class="nav-item">
+        <RouterLink class="nav-link" active-class="active" to="/nested/two">Nested Two</RouterLink>
+      </li>
+    </ul>
+    <hr class="mt-5" />
+    <RouterView></RouterView>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/layout/TheHeader.vue
+++ src/layout/TheHeader.vue
@@ -25,6 +25,9 @@
             <li class="nav-item">
               <RouterLink class="nav-link" active-class="active" to="/boards">List</RouterLink>
             </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" active-class="active" to="/nested">Nested</RouterLink>
+            </li>
           </ul>
           <div class="d-flex" role="search">
             <button class="btn btn-primary" type="button" @click="boardCreatePage">Create</button>
