83c77895e82b80bb6d22173f7743ce8dcf9e199b
LifeIsOne <didtmdgh258@gmail.com>
2025-06-04 20:07:52 +0900

    [FEAT] 게시글 수정 - Axios, JSON-Server

--- src/views/boards/BoardEditView.vue
+++ src/views/boards/BoardEditView.vue
@@ -2,14 +2,14 @@
   <div>
     <h1>Board Edit</h1>
     <hr />
-    <form @submit.prevent>
+    <form @submit.prevent="boardEdit">
       <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label">Title</label>
-        <input type="text" class="form-control" id="title" />
+        <input v-model="boardForm.title" type="text" class="form-control" id="title" />
       </div>
       <div class="mb-3">
         <label for="content" class="form-label">Content</label>
-        <textarea class="form-control" id="content" rows="3"></textarea>
+        <textarea v-model="boardForm.content" class="form-control" id="content" rows="3"></textarea>
       </div>
       <div class="pt-3 d-flex gap-2">
         <button type="button" class="btn btn-outline-danger" @click="boardDetailPage">
@@ -22,10 +22,37 @@
 </template>
 
 <script setup>
+import { getBoardById, updateBoard } from '@/api/boards'
+import { ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 
 const route = useRoute()
 const router = useRouter()
+const boardId = route.params.boardId
+const boardForm = ref({
+  title: null,
+  content: null,
+})
+
+// 1. 조회
+const fetchBoard = async () => {
+  const { data } = await getBoardById(boardId)
+  setBoardForm(data)
+}
+const setBoardForm = ({ title, content }) => {
+  boardForm.value.title = title
+  boardForm.value.content = content
+}
+fetchBoard()
+
+const boardEdit = async () => {
+  try {
+    await updateBoard(boardId, { ...boardForm.value })
+    router.push({ name: 'BoardDetail', params: { boardId } })
+  } catch (err) {
+    console.error(err)
+  }
+}
 
 const boardDetailPage = () => {
   router.push(`/boards/${route.params.id}`)
